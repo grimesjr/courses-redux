c061be9008e72a67c9a692fe537c63884a379b49
Jimmy Lin <jimmylin@Lillians-MacBook-Pro-2018.local>
2020-07-31 03:47:35 -0700

    fix windows to window

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,7 +4,7 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
   const composeEnhancers =
-    windows.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for redux dev tool
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for redux dev tool
   return createStore(
     rootReducer,
     initialState,
