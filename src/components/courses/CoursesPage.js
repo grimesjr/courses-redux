ff46043123dfe9e3d8e198223f7961f9e6bb144a
Jimmy Lin <jimmylin@Lillians-MacBook-Pro-2018.local>
2020-08-01 02:44:24 -0700

    add thunk, loadCourses, convert coursePage to container

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,40 +5,19 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: '',
-    },
-  };
-
-  handleChange = (e) => {
-    const course = { ...this.state.course, title: e.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-    this.setState({
-      course: { title: '' },
+  componentDidMount() {
+    this.props.actions.loadCourses().catch((error) => {
+      alert('loading courses error' + error);
     });
-  };
-
+  }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map((courses) => (
           <div key={courses.title}>{courses.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
